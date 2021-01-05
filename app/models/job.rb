15e22de2bf985dba5e400b76ddc854986e63dedf
nagaraju-exalogic <nagaraju@exalogic.in>
2021-01-05 22:01:00 +0530

    validation for job form

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,14 +1,14 @@
 class Job < ApplicationRecord
 	has_rich_text :description
 	has_rich_text :information
-	validates :title, presence:{message: "please enter job title"}
-	validates :company_name, presence:{message: "please enter company_name"}
-	validates :url, presence:{message: "please enter company url"}
-	validates :country, presence:{message: "please enter country"}
-	validates :state, presence:{message: "please enter state"}
-	validates :city, presence:{message: "please enter city"}
-	validates :description, presence:{message: "please enter job description"}
-	validates :information, presence:{message: "please enter job information"}
-	validates :skills, presence:{message: "please select skills"}
+	validates :title, presence:{message: "Please enter job title"}
+	validates :company_name, presence:{message: "Please enter company name"}
+	validates :url, presence:{message: "Please enter company url"}
+	validates :country, presence:{message: "Please enter country"}
+	validates :state, presence:{message: "Please enter state"}
+	validates :city, presence:{message: "Please enter city"}
+	validates :description, presence:{message: "Please enter job description"}
+	validates :information, presence:{message: "Please enter job information"}
+	validates :skills, presence:{message: "Please select skills"}
 
 end
